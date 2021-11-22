ac0beab934296e9b58757e9ac3747a43bf78ce6b
liaopengjun <1337404942@qq.com>
2021-11-22 23:35:19 +0800

    111

---
+++ src/components/common/Aside.vue
@@ -0,0 +1,66 @@
+<template>
+    <el-menu  class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+    <h3>通用后台管理系统</h3>
+    <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+    </el-menu-item>
+    <el-submenu index="1">
+        <template slot="title">
+        <i class="el-icon-location"></i>
+        <span slot="title">导航一</span>
+        </template>
+        <el-menu-item-group>
+        <span slot="title">分组一</span>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
+        </el-menu-item-group>
+        <el-menu-item-group title="分组2">
+        <el-menu-item index="1-3">选项3</el-menu-item>
+        </el-menu-item-group>
+        <el-submenu index="1-4">
+        <span slot="title">选项4</span>
+        <el-menu-item index="1-4-1">选项1</el-menu-item>
+        </el-submenu>
+    </el-submenu>
+    </el-menu>
+</template>
+<style>
+  .el-menu-vertical-demo:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+</style>
+<script>
+  export default {
+    data() {
+      return {
+        isCollapse: false,
+        meun :[
+           {
+            path:"/home",
+            name:"home",
+            label:"首页",
+            icon:"首页",
+            url:"Home/Home"
+          },
+          {
+            path:"/user",
+            name:"user",
+            label:"用户管理",
+            icon:"用户管理",
+            url:"UserManage/UserManage"
+          }
+        ]
+      };
+    },
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
---
+++ src/view/Main.vue
@@ -0,0 +1,38 @@
+<template>
+    <el-container>
+        <el-aside width="200px"><common-aside></common-aside></el-aside>
+        <el-container>
+            <el-header>Header</el-header>
+            <el-main>Main</el-main>
+            <el-footer>Footer</el-footer>
+        </el-container>
+    </el-container>
+</template>
+<script>
+import CommonAside from "../components/common/Aside.vue"
+export default {
+    name:"Main",
+    components:{CommonAside}
+}
+</script>
+<style  scoped>
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+</style>
